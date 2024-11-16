042b10b035b1fc5fcd5e30efcfd8e0881405a3ba
0xjaqbek <jaqbek.eth@gmail.com>
2024-11-17 00:50:56 +0100

    vite and index back

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,18 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    headers: {
-      // Remove CSP during development
-    }
-  }
+ base: '/portfolio/',
+ plugins: [react()],
+ server: {
+   headers: {
+     'Content-Security-Policy': `
+       default-src 'self';
+       script-src 'self' 'unsafe-inline' 'unsafe-eval' 'wasm-unsafe-eval' chrome-extension: blob:;
+       style-src 'self' 'unsafe-inline';
+       img-src 'self' data: blob: chrome-extension:;
+       connect-src 'self' https://api.emailjs.com chrome-extension:;
+       font-src 'self';
+     `.replace(/\s+/g, ' ').trim()
+   }
+ }
 });
