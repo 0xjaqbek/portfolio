43f0a62130c5d3eb64b35836fd3108fa32527d9c
0xjaqbek <jaqbek.eth@gmail.com>
2024-11-17 18:47:26 +0100

    images

--- src/imageConstants.jsx
+++ src/imageConstants.jsx
@@ -1,17 +1,17 @@
 // imageConstants.js
 export const auditSystemImages = [
-    '../public/a/1.jpg',
-    '../public/a/2.jpg',
-    '../public/a/3.jpg',
-    '../public/a/4.jpg',
-    '../public/a/5.jpg'
+    './a/1.jpg',
+    './a/2.jpg',
+    './a/3.jpg',
+    './a/4.jpg',
+    './a/5.jpg'
   ];
   
   export const constructionSystemImages = [
-    '../public/b/1.jpg',
-    '../public/b/2.jpg',
-    '../public/b/3.jpg',
-    '../public/b/4.jpg',
-    '../public/b/5.jpg',
-    '../public/b/6.jpg'
+    './b/1.jpg',
+    './b/2.jpg',
+    './b/3.jpg',
+    './b/4.jpg',
+    './b/5.jpg',
+    './b/6.jpg'
   ];
